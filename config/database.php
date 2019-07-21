f0df4d1012de373562a10c5938c8e3202fe53fcd
unknown <Olurin anjola>
2019-07-21 23:33:52 +0100

    first commit

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'dpzb2z17b4zmkzel'),
+            'username' => env('DB_USERNAME', 'ejgrqe8rf8amw9eu'),
+            'password' => env('DB_PASSWORD', 'n1v2j2v1acy79cxd'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',

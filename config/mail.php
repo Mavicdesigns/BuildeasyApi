05b542b903c760efa04cc0604867f244ebcfc84e
unknown <Olurin anjola>
2019-07-22 18:44:28 +0100

    first commit

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'buildeasy@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'sanmisola2019@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Buildeasy'),
     ],
 

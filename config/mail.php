4bca1f1dc925df6efc29638bde67767d59d6b677
unknown <Olurin anjola>
2019-07-22 18:29:14 +0100

    first commit

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'Buildeasy.xyz'),
+        'address' => env('MAIL_FROM_ADDRESS', 'www.Buildeasy.xyz'),
         'name' => env('MAIL_FROM_NAME', 'Buildeasy'),
     ],
 

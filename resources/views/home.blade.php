12b1ba2a6db4bd3dbed11fc5ce40ae6a9e5ab5d3
unknown <Olurin anjola>
2019-07-22 19:12:12 +0100

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,18 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
+                <div class="panel-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
+                        <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    You are logged in!
+
+
+                    <h2> Name  - <strong>{{Auth::user()->name}}</strong></h2>
+                    <h2>Email - <strong>{{Auth::user()->email}}</strong></h2>
+                    <h2>Api-Key - <strong>{{Auth::user()->api_key}}</strong></h2>
                 </div>
             </div>
         </div>

d08f227167480e830faeb1dc537ca1a81e424a38
unknown <Olurin anjola>
2019-07-25 03:09:14 +0100

    M

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,9 +16,9 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin')
+            ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization')
-            ->header('Allow-Headers: X-CSRF-Token');
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-CSRF-Token, Authorization');
     }
 }
